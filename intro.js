27b273c13c7089e35a6512832c584bb241a3fc13
hcontrerasg <h.contrerasg@hotmail.com>
2023-08-07 00:03:28 +0200

    tercer avance Javascript

--- intro.js
+++ intro.js
@@ -1,76 +1,81 @@
 
 let listaParticipantes =[];
-
-const objparticipante ={
-    id:"",
-    nombre: "",
-    puesto: "",
-    pais:""
-}
+const objparticipante = {
+    id:""
+    ,nombre: ""
+    ,puesto: ""
+    ,pais: ""
+};
 
 let editando = false;
 
 const formulario = document.querySelector ("#formulario");
 const nombreInput = document.querySelector ("#nombre");
-const equipoInput = document.querySelector ("#equipo");
+const puestoInput = document.querySelector ("#puesto");
 const paisInput = document.querySelector ("#pais");
 const btnAgregar = document.querySelector ("#btnAgregar");
 
 formulario.addEventListener("submit", validarFormulario);
 
-function validarFormaulario (e){
-    e.preventdefault();
+function validarFormulario (e) {
+    e.preventDefault();
 
-    if (nombreInput.value ===""|| equipoInput.value === ""||paisInput.value===""){
+    //resto del codigo de validación
+
+    if (nombreInput.value ===""|| puestoInput.value === ""||paisInput.value===""){
         alert("Todos los campos son obligatorios.");
         return;
 }
 
-if (editando){
+if (editando) {
     //editarParticipante ();
     editando =false;
 } else {
-    objparticipante.id = date.now();
+    objparticipante.id = Date.now();
     objParticipante.nombre = nombreInput.value;
-    objParticipante.equipo = equipoInput.value;
+    objParticipante.puesto = puestoInput.value;
     objParticipante.pais = paisInput.value;
 
     agregarParticipante();
 
-}
-}
-
-function agregarParticipante (){
-    listaParticipantes.push({...objParticipante});
-
-    mostrarParticipantes ();
+    }
 }
 
-function mostrarParticipantes (){
+function agregarParticipante() {
+    listaParticipantes.push({...objParticipante
+    });
+
+    mostrarParticipantes();
+}
+
+function mostrarParticipantes() {
     const divParticipantes = document.querySelector(".div-participantes")
 
-listaParticipantes.forEach (participante =>{
-    const {id, nombre, equipo,pais} = participante;
+listaParticipantes.forEach (participante => {
+    const {
+        id, nombre, puesto,pais
+    } = participante;
 
     const parrafo = document.createElement ("p");
-    parrafo.textContent = "${id} - ${nombre} - ${equipo} - ${pais} - "; 
-    parrafo.dataset =id;
+    parrafo.textContent = `${id} - ${nombre} - ${puesto} - ${pais} -`; 
+    parrafo.dataset.id =id;
 
     const editarBoton =document.createElement("button");
     //editarBoton.onclick = () => cargarParticipante(participante);
     editarBoton.textContent ="Editar";
     editarBoton.classList.add("btn","btn-editar");
-    parrafo.append(editarBoton);
+    parrafo.appendChild(editarBoton);
 
     const eliminarBoton =document.createElement("button");
     //editarBoton.onclick = () => cargarParticipante(participante);
     eliminarBoton.textContent ="Eliminar";
     eliminarBoton.classList.add("btn","btn-eliminar");
-    parrafo.append(eliminarBoton);
+    parrafo.appendChild(eliminarBoton);
 
     const hr = document.createElement("hr");
 
     divParticipantes.appendChild(parrafo);
     divParticipantes.appendChild(hr);
 
-}) ;
+});
+}
